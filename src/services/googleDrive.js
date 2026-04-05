a0fb1adcf140d13c8f992b3d42a63db0e0ebb5a9
Tiago Daniel Jacobs <tiago.jacobs@gmail.com>
2026-04-05 04:10:21 +0000

    Use direct download link for Google Drive shared files

--- src/services/googleDrive.js
+++ src/services/googleDrive.js
@@ -70,5 +70,5 @@ export async function makeFileShareable(accessToken, fileId) {
  * @returns {string} Shareable URL
  */
 export function getShareableLink(fileId) {
-  return `https://drive.google.com/file/d/${fileId}/view?usp=sharing`
+  return `https://drive.google.com/uc?export=download&id=${fileId}`
 }
